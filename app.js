8abd661dd3b0465a70d6fca622e8594e9dbb58ff
MhZ9191 <hegic91@gmail.com>
2026-02-27 15:15:20 +0100

    Add first response

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
+console.log("Hello ", process.env.username);
 
+const express = require("express");
+const app = express();
+const port = 3000;
+const appUrl = "http://127.0.0.1:3000";
+
+app.get("/", (req, res) => {
+  res.send("welcome");
+});
+
+app.listen(port, () => {
+  console.log(`In ascolto su ${appUrl}`);
+});
