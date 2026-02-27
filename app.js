d296e8d12fb34a9e9bb637f93d6eafadf34cdfe2
MhZ9191 <hegic91@gmail.com>
2026-02-27 15:43:37 +0100

    Add app.use & route "/bacheca"

--- app.js
+++ app.js
@@ -5,8 +5,50 @@ const app = express();
 const port = 3000;
 const appUrl = "http://127.0.0.1:3000";
 
+const dataValues = {
+  results: [
+    {
+      title: "Post uno",
+      content: "Hello World",
+      image: "ciambellone.jpeg",
+      tags: ["one", "two"],
+    },
+    {
+      title: "Post due",
+      content: "Hello World",
+      image: "cracker_barbabietole.jpeg",
+      tags: ["one", "two"],
+    },
+    {
+      title: "Post tre",
+      content: "Hello World",
+      image: "pane_fritto_dolce.jpeg",
+      tags: ["one", "two"],
+    },
+    {
+      title: "Post quattro",
+      content: "Hello World",
+      image: "pasta_barbabietola.jpeg",
+      tags: ["one", "two"],
+    },
+    {
+      title: "Post cinque",
+      content: "Hello World",
+      image: "torta_paesana.jpeg",
+      tags: ["one", "two"],
+    },
+  ],
+  success: true,
+};
+
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
-  res.send("welcome");
+  res.send("Server del mio blog");
+});
+
+app.get("/bacheca", (req, res) => {
+  res.json(dataValues);
 });
 
 app.listen(port, () => {
